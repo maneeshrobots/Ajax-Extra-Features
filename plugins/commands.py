8b24ddbe729bed80dad338e114db7717219b4860
maneeshrobots <109596103+maneeshrobots@users.noreply.github.com>
2022-08-22 13:58:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,7 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN', url='https://t.me/hd_moviesrequest') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('JOIN', url='https://t.me/HDMLinkz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
